3869e3b2ceebe04647c49be512ff1a49cf5abf82
Ale Damilola <damidigi@yahoo.com>
2019-11-08 21:38:55 +0100

    Remove unused files

--- search/search.rb
+++ search/search.rb
@@ -4,11 +4,10 @@ require 'rest-client'
 
 # Search
 class Search
-
   def search
     puts 'What do you want to search for?'
     search_input = gets.chomp.to_s
-    search_input.gsub(' ','+')
+    search_input.gsub(' ', '+')
     response = RestClient.get "http://bing.com/search?q=#{search_input}"
     File.open(search_input, 'w') do |file|
       file.write(response.body)
