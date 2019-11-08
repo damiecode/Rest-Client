53f41ca4029620815a1adb24bcc51cdcd8c7dc23
Marcos Medeiros <mvsmdrs@protonmail.com>
2019-11-08 17:28:58 -0300

    Fix search method

--- search/search.rb
+++ search/search.rb
@@ -1,20 +1,17 @@
 # frozen_string_literal: true
 
 require 'rest-client'
+
 # Search
 class Search
-  def initialize
-    @@url = 'http://bing.com'
-  end
 
   def search
     puts 'What do you want to search for?'
     search_input = gets.chomp.to_s
-    response = RestClient.get @@url, { :q => search_input }
+    search_input.gsub(' ','+')
+    response = RestClient.get "http://bing.com/search?q=#{search_input}"
     File.open(search_input, 'w') do |file|
-      file.write(response)
+      file.write(response.body)
     end
   end
 end
-
-search_obj = Search.new 
